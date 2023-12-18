7824051884d6f8f05501a58da88cfd6c157f1cfb
vitaliystorchous <50746287+vitaliystorchous@users.noreply.github.com>
2023-12-18 22:34:39 +0100

    ci: verisioning (#7)
    * ci: versioning
    
    * chore: upgrade @vitaliystorchous/require-env-var
    
    * chore: v1.0.1
    
    * fix: PORT constant to handle possible NaN
    
    * build: replace package require-env-var with env-var

--- test/consts/FromEnvVars.ts
+++ test/consts/FromEnvVars.ts
@@ -1,4 +1,10 @@
 import "dotenv/config"
+import * as env from "env-var"
 
-export const SERVER_HOST = process.env.SERVER_HOST || '127.0.0.1'
-export const SERVER_PORT = process.env.SERVER_PORT || 3000
+
+export const SERVER_HOST = env.get('SERVER_HOST')
+  .default('127.0.0.1')
+  .asString()
+export const SERVER_PORT = env.get('SERVER_PORT')
+  .default(3000)
+  .asPortNumber()
